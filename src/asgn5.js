3ee97aba7ea2ec3fb330df9bf614c56cf470a917
Raheb Games <122946796+rahebgames@users.noreply.github.com>
2026-05-30 23:28:29 -0700

    commit camera

--- src/asgn5.js
+++ src/asgn5.js
@@ -1,28 +1,54 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const fov = 75;
+const aspect = 2;  // the canvas default
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const loader = new THREE.TextureLoader();
-const texture = loader.load('../assets/dirt.jpg');
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('../assets/dirt.jpg');
 texture.colorSpace = THREE.SRGBColorSpace;
 
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshPhongMaterial({
   map: texture,
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+const gltfLoader = new GLTFLoader();
+const gltf = await gltfLoader.loadAsync('../assets/monster.glb');
+scene.add(gltf.scene);
+
+const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+camera.position.z = 10;
+function updateCamera() {
+  camera.updateProjectionMatrix();
+}
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 5, 0);
+controls.update();
 
 function animate(time) {
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 1000;
+  time *= 0.001;  // convert time to seconds
+
+  cube.rotation.x = time;
+  cube.rotation.y = time;
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
